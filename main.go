06a416c29e7ec6f2257aee79be9f7fc70a9499e5
Mithun Prem <mithun@hamon.in>
2023-09-25 22:26:55 +0530

    variables in go

--- main.go
+++ main.go
@@ -4,6 +4,6 @@ func main () {
 
 	fmt.Println ("Wlcome to our conference booking application")
 
-	var conferenceName = "Go conference"
+	var conferenceName = "Go conference" //variables in go
 	fmt.Println(conferenceName)
 }
