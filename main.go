fd8f0955356faeb68f7594915e356e937bf8f2e1
Mithun Prem <mithun@hamon.in>
2023-09-26 15:39:00 +0530

    Booking app

--- main.go
+++ main.go
@@ -3,24 +3,36 @@ package main
 import "fmt"
 
 func main() {
-	conferenceName := "Go conference" //variables in go thus can be used for only var and not for const 
+	conferenceName := "Go conference" //variables in go thus can be used for only var and not for const
 	const conferenceTickets = 50
-	var remainingTickets = 50
+	var remainingTickets uint = 50
 	fmt.Printf("conferenceTicket is %T ,remaining %T,conference name %T\n", conferenceTickets, remainingTickets, conferenceName)
 	fmt.Printf("Welcome to our %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v and from that %v remaining\n", conferenceTickets, remainingTickets)
 
-	var userName string
-	var userTickets int
+	var firstname string
+	var lastname string
+
+	var email string
+	var userTickets uint
+
 	//ask the user for name
-	userName = "Mithun"
+
 	userTickets = 2
 
 	fmt.Println("Enter your first name: ")
-	fmt.Scan(&userName) //used to take the userinput direct from the user
-	
-	
-	
-	// fmt.Println(&userName)
-	fmt.Printf("User %v,Booked %v this many tickets", userName, userTickets)
+	fmt.Scan(&firstname) //used to take the userinput direct from the user
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastname)
+	fmt.Println("Enter your email: ")
+
+	fmt.Scan(&email)
+
+	fmt.Println("Enter your number of tickets: ")
+	fmt.Scan(&userTickets)
+	remainingTickets = remainingTickets - userTickets
+	// fmt.Println(&firstname)
+	fmt.Printf("Thankyou %v %v for booking %v tickets . You will receive confirmation email at %v\n",firstname,lastname,userTickets,email)
+	fmt.Printf("%v remaining tickets in conference %v",remainingTickets,conferenceName)
 }
