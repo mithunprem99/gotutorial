d80e0d99d8e10cb4d470f4345254c514c50d7bee
Mithun Prem <mithun@hamon.in>
2023-09-26 23:35:40 +0530

    forloop

--- main.go
+++ main.go
@@ -12,30 +12,6 @@ func main() {
 
 	for {
 
-<<<<<<< Updated upstream
-	var email string
-	var userTickets uint
-
-	//ask the user for name
-
-	userTickets = 2
-
-	fmt.Println("Enter your first name: ")
-	fmt.Scan(&firstname) //used to take the userinput direct from the user
-
-	fmt.Println("Enter your last name: ")
-	fmt.Scan(&lastname)
-	fmt.Println("Enter your email: ")
-
-	fmt.Scan(&email)
-
-	fmt.Println("Enter your number of tickets: ")
-	fmt.Scan(&userTickets)
-	remainingTickets = remainingTickets - userTickets
-	// fmt.Println(&firstname)
-	fmt.Printf("Thankyou %v %v for booking %v tickets . You will receive confirmation email at %v\n",firstname,lastname,userTickets,email)
-	fmt.Printf("%v remaining tickets in conference %v",remainingTickets,conferenceName)
-=======
 		var firstname string
 		var lastname string
 	
@@ -76,5 +52,4 @@ func main() {
 		fmt.Printf("%v remaining tickets in conference %v\n",remainingTickets,conferenceName)
 	}
 
->>>>>>> Stashed changes
 }
