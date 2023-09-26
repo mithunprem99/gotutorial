3522a19d49a4e79206640b6036c11e8b439c42dd
Mithun Prem <mithun@hamon.in>
2023-09-26 14:29:24 +0530

    Variable updated '

--- main.go
+++ main.go
@@ -3,10 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go conference" //variables in go
+	conferenceName := "Go conference" //variables in go thus can be used for only var and not for const 
 	const conferenceTickets = 50
 	var remainingTickets = 50
-	fmt.Printf("Welcome to our %v booking application\n",conferenceName)
-	fmt.Printf("We have total of %v and from that %v remaining\n",conferenceTickets,remainingTickets)
+	fmt.Printf("conferenceTicket is %T ,remaining %T,conference name %T\n", conferenceTickets, remainingTickets, conferenceName)
+	fmt.Printf("Welcome to our %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v and from that %v remaining\n", conferenceTickets, remainingTickets)
 
+	var userName string
+	var userTickets int
+	//ask the user for name
+	userName = "Mithun"
+	userTickets = 2
+	fmt.Printf("User %v,Booked %v this many tickets", userName, userTickets)
 }
