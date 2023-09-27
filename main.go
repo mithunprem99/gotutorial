24a0e9273c9bd7b7ba4b2f5ab238eeacc7affffa
Mithun Prem <mithun@hamon.in>
2023-09-27 15:34:09 +0530

    Need to run the file with command go run . because it need to execute both file

--- main.go
+++ main.go
@@ -96,9 +96,4 @@ func printFirstName(booking []string)  {
 			}
 }
 
-func isValidateUserInput(firstname string,lastname string,email string,userTickets uint) (bool, bool,bool) {
-	isValidName := len(firstname) >= 2 && len(lastname) >= 2
-	isValidEmail := strings.Contains(email, "@")
-	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
-	return isValidName,isValidEmail,isValidTicketNumber
-}
+
