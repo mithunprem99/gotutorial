53099655850a4f6e16d9049e8003d0f8034be496
Mithun Prem <mithun@hamon.in>
2023-09-27 12:05:06 +0530

    Forloop

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	conferenceName := "Go conference" //variables in go thus can be used for only var and not for const
@@ -14,42 +17,45 @@ func main() {
 
 		var firstname string
 		var lastname string
-	
+
 		var email string
 		var userTickets uint
-		var booking [] string
-	
-	
+		var booking []string
+
 		//ask the user for name
-	
+
 		userTickets = 2
-	
+
 		fmt.Println("\nEnter your first name: ")
 		fmt.Scan(&firstname) //used to take the userinput direct from the user
-	
+
 		fmt.Println("\nEnter your last name: ")
 		fmt.Scan(&lastname)
 		fmt.Println("Enter your email: ")
-	
+
 		fmt.Scan(&email)
-	
+
 		fmt.Println("Enter your number of tickets: ")
 		fmt.Scan(&userTickets)
-		
+
 		remainingTickets = remainingTickets - userTickets
 		// booking[0] = firstname + " " + lastname
-		booking = append(booking, firstname + " " + lastname)
-		
-		fmt.Printf("The whole slice %v\n",booking)
-		fmt.Printf("The firstvalue slice %v\n",booking[0])
-		fmt.Printf("slice type%T\n",booking)
-		fmt.Printf("slice type%v\n",len(booking))
-	
-	
-	
+		booking = append(booking, firstname+" "+lastname)
+
+		fmt.Printf("The whole slice %v\n", booking)
+		fmt.Printf("The firstvalue slice %v\n", booking[0])
+		fmt.Printf("slice type%T\n", booking)
+		fmt.Printf("slice type%v\n", len(booking))
+
 		// fmt.Println(&firstname)
-		fmt.Printf("Thankyou %v %v for booking %v tickets . You will receive confirmation email at %v\n",firstname,lastname,userTickets,email)
-		fmt.Printf("%v remaining tickets in conference %v\n",remainingTickets,conferenceName)
+		fmt.Printf("Thankyou %v %v for booking %v tickets . You will receive confirmation email at %v\n", firstname, lastname, userTickets, email)
+		fmt.Printf("%v remaining tickets in conference %v\n", remainingTickets, conferenceName)
+		firstnames := []string{}
+		for _, booking := range booking {
+			var name = strings.Fields(booking)
+			firstnames = append(firstnames, name[0])
+		}
+		fmt.Printf("The first name of the bookings are : %v\n", firstnames)
 	}
 
 }
